0b344fa4ae7b11c44bac3bd5e0475003bc682ead
ilopX <ilopXgroup@gmail.com>
2020-08-12 14:54:37 +0300

    fix: do not removing main.dart.js.map

--- deploy_web_app.bat
+++ deploy_web_app.bat
@@ -1,8 +1,9 @@
 cd example
 call flutter build web
 cd build\web
-call del main.dart.js.map
+:: call del main.dart.js.map
 call git init
 call git add .
 call git commit -m "this commit automatically generated from push script"
-call git push origin master
+call git remote add origin https://github.com/ilopX/flutter_nice_gradient_platform_web
+call git push origin master --force
